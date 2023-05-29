ba6b2ad1ebebb5a780d4ff51237b4a508d424cb1
HMETAO <1430271732@qq.com>
2023-05-29 08:27:47 +0800

    fix(ui): 优化ui

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,16 +1,22 @@
 <template>
-    <div class=" h-full w-full flex home-container">
-        <el-row class=" home-row h-full w-full">
-            <el-col :span="3" :xs="0" class="animate__animated animate__bounceInLeft">
-                <AsideMenu></AsideMenu>
-            </el-col>
-            <el-col :span="21" :xs="24">
-                <div class="p-3 w-full h-full animate__animated animate__bounceInRight">
-                    <router-view></router-view>
-                </div>
-            </el-col>
-        </el-row>
-    </div>
+  <div class=" h-full w-full flex home-container">
+    <el-row class=" home-row h-full w-full">
+      <el-col :span="3" :xs="0">
+        <transition enter-active-class="animate__animated animate__bounceInLeft" appear
+                    appear-active-class="animate__animated animate__bounceInLeft">
+          <AsideMenu></AsideMenu>
+        </transition>
+      </el-col>
+      <el-col :span="21" :xs="24">
+        <transition enter-active-class="animate__animated animate__bounceInRight" appear
+                    appear-active-class="animate__animated animate__bounceInRight">
+          <div class="p-3 w-full h-full ">
+            <router-view></router-view>
+          </div>
+        </transition>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <script setup lang="ts">
 import {ref, reactive, toRef, computed} from 'vue';
