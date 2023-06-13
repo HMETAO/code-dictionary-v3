23f587694f0eabd72b934d6a5fd236099419f048
HMETAO <1430271732@qq.com>
2023-06-13 09:54:48 +0800

    fix(other): 调增UI

--- src/components/other/OtherQuickPlug.vue
+++ src/components/other/OtherQuickPlug.vue
@@ -1,25 +1,23 @@
 <template>
-    <div class="w-full h-full">
-        <el-card class="card-box">
-            <template #header>
-                <div class="card-header">
-                    <svg class="icon icon-font-size-b" aria-hidden="true">
-                        <use xlink:href="#icon-quick-copy"></use>
-                    </svg>
-                    <div class="card-title">
-                        快捷插件
-                    </div>
-                </div>
-            </template>
-            <el-row :gutter="12">
-                <el-col :span="8">
-                    <el-card  :body-style="{ padding: '10px' }">
-                        <SSH/>
-                    </el-card>
-                </el-col>
-            </el-row>
-        </el-card>
-    </div>
+  <div class="w-full h-full">
+    <el-card class="card-box">
+      <template #header>
+        <div class="card-header">
+            <span class="iconfont  icon-quick-copy icon-font-size-b"></span>
+            <div class="card-title">
+              快捷插件
+            </div>
+          </div>
+      </template>
+      <el-row :gutter="12">
+        <el-col :span="8">
+          <el-card :body-style="{ padding: '10px' }">
+            <SSH/>
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
 </template>
 <script setup lang="ts">
 import {ref} from 'vue';
@@ -30,9 +28,10 @@ import SSH from "./SSH.vue";
 .card-box {
   .card-header {
     display: flex;
+    flex-direction: row;
     align-items: center;
     width: 100%;
-    height: 100%;
+    height: 1em;
 
     .card-title {
       margin-left: 10px;
--- src/components/other/SSH.vue
+++ src/components/other/SSH.vue
@@ -1,34 +1,32 @@
 <template>
-    <div class="w-full h-full">
-        <el-button text class="quick-item" @click="sshClickEventFunction">
-            <svg class="icon icon-font-size-m" aria-hidden="true">
-                <use xlink:href="#icon-SSHmiyue"></use>
-            </svg>
-            ssh 连接
-        </el-button>
-        <el-dialog v-model="dialogVisible" center title="SSH 配置" width='35%' class="ssh-dialog">
-            <div>
-                <el-form :model='sshForm' :rules="rules">
-                    <el-form-item label='host：' :label-width='formLabelWidth' prop="host">
-                        <el-input v-model='sshForm.host' autocomplete='off'></el-input>
-                    </el-form-item>
-                    <el-form-item label='username：' :label-width='formLabelWidth' prop="username">
-                        <el-input v-model='sshForm.username' autocomplete='off'></el-input>
-                    </el-form-item>
-                    <el-form-item label='password：' :label-width='formLabelWidth' prop="password">
-                        <el-input v-model='sshForm.password' autocomplete='off' show-password></el-input>
-                    </el-form-item>
-                    <el-form-item label='port：' :label-width='formLabelWidth' prop="port">
-                        <el-input v-model='sshForm.port' autocomplete='off'></el-input>
-                    </el-form-item>
-                </el-form>
-            </div>
-            <template #footer>
-                <el-button @click='dialogVisible = false'>取 消</el-button>
-                <el-button type='primary' @click='dialogClickEventFunction'>确 定</el-button>
-            </template>
-        </el-dialog>
-    </div>
+  <div class="w-full h-full">
+    <el-button text class="quick-item" @click="sshClickEventFunction">
+      <span class="iconfont  icon-SSHmiyue icon-font-size-m"></span>
+      ssh 连接
+    </el-button>
+    <el-dialog v-model="dialogVisible" center title="SSH 配置" width='35%' class="ssh-dialog">
+      <div>
+        <el-form :model='sshForm' :rules="rules">
+          <el-form-item label='host：' :label-width='formLabelWidth' prop="host">
+            <el-input v-model='sshForm.host' autocomplete='off'></el-input>
+          </el-form-item>
+          <el-form-item label='username：' :label-width='formLabelWidth' prop="username">
+            <el-input v-model='sshForm.username' autocomplete='off'></el-input>
+          </el-form-item>
+          <el-form-item label='password：' :label-width='formLabelWidth' prop="password">
+            <el-input v-model='sshForm.password' autocomplete='off' show-password></el-input>
+          </el-form-item>
+          <el-form-item label='port：' :label-width='formLabelWidth' prop="port">
+            <el-input v-model='sshForm.port' autocomplete='off'></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <template #footer>
+        <el-button @click='dialogVisible = false'>取 消</el-button>
+        <el-button type='primary' @click='dialogClickEventFunction'>确 定</el-button>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 <script setup lang="ts">
 import {reactive, ref} from 'vue';
@@ -44,37 +42,35 @@ const dialogVisible = ref<boolean>(false)
 const sshForm = ref<SSHForm>({host: "", port: 22, username: 'root'})
 // 点击开启ssh事件回调
 const sshClickEventFunction = () => {
-    dialogVisible.value = true;
+  dialogVisible.value = true;
 }
 // 表单规则
 const rules = reactive<FormRules>({
-    host: [
-        {required: true, message: '请输入连接主机的host', trigger: 'blur'}
-    ],
-    username: [
-        {required: true, message: '请输入连接登录主机用户', trigger: 'blur'}
-    ],
-    port: [
-        {required: true, message: '请输入port', trigger: 'blur'}
-    ],
-    password: [
-        {required: true, message: '请输入密码', trigger: 'blur'}
-    ]
+  host: [
+    {required: true, message: '请输入连接主机的host', trigger: 'blur'}
+  ],
+  username: [
+    {required: true, message: '请输入连接登录主机用户', trigger: 'blur'}
+  ],
+  port: [
+    {required: true, message: '请输入port', trigger: 'blur'}
+  ],
+  password: [
+    {required: true, message: '请输入密码', trigger: 'blur'}
+  ]
 })
 const router = useRouter()
 // dialog确认按钮事件回调
 const dialogClickEventFunction = () => {
-    // 设置ssh
-    setSSH(sshForm.value).then(() => {
-        router.push({name: 'term'})
-    })
+  // 设置ssh
+  setSSH(sshForm.value).then(() => {
+    router.push({name: 'term'})
+  })
 }
 </script>
 <style scoped lang="less">
 .quick-item {
   width: 100%;
   height: 100%;
-
-
 }
 </style>
