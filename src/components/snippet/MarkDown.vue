29a8ccdae181155967ab2ef86eb568bfb4faa30b
HMETAO <1430271732@qq.com>
2023-06-13 19:31:28 +0800

    fix(markdown): 修复markdown组件事件卸载错误

--- src/components/snippet/MarkDown.vue
+++ src/components/snippet/MarkDown.vue
@@ -1,60 +1,58 @@
 <template>
-    <div class="markdown-box h-full">
-        <MdEditor v-model="snippetForm.snippet"
-                  @onSave="saveMarkdownEventFunction"
-                  @onUploadImg="uploadImgEventFunction"
-                  :toolbars-exclude="['github']"
-                  :no-prettier="true"
-                  :showCodeRowNumber="true">
-        </MdEditor>
-    </div>
+  <div class="markdown-box h-full">
+    <MdEditor v-model="snippetForm.snippet"
+              @onSave="saveMarkdownEventFunction"
+              @onUploadImg="uploadImgEventFunction"
+              :toolbars-exclude="['github']"
+              :no-prettier="true"
+              :showCodeRowNumber="true">
+    </MdEditor>
+  </div>
 </template>
 <script setup lang="ts">
-import {getCurrentInstance, onBeforeMount} from 'vue';
+import {getCurrentInstance, onBeforeUnmount} from 'vue';
 import MdEditor from 'md-editor-v3';
-import {SNIPPET_GET_EVENT} from "../../constants/eventConstants";
-import {useStateStore} from "../../store";
+import {SNIPPET_GET_EVENT} from "@/constants/eventConstants";
+import {useStateStore} from "@/store";
 import {storeToRefs} from "pinia";
-import {SnippetType} from "../../type/snippetType";
-import {updateSnippet, uploadImg} from "../../api/snippet";
-import {successMessage} from "../../utils/baseMessage";
+import {SnippetType} from "@/type/snippetType";
+import {updateSnippet, uploadImg} from "@/api/snippet";
+import {successMessage} from "@/utils/baseMessage";
 
 const stateStore = useStateStore()
 const {snippetForm} = storeToRefs(stateStore)
 
 //点击保存markdown按钮事件回调
 const saveMarkdownEventFunction = () => {
-    // 判断是更新还是新增
-    if (snippetForm.value.id) {
-        updateSnippet(snippetForm.value)
-        successMessage("更新成功")
-    } else {
-        stateStore.snippetDialogVisible = true
-    }
-
-
+  // 判断是更新还是新增
+  if (snippetForm.value.id) {
+    updateSnippet(snippetForm.value)
+    successMessage("更新成功")
+  } else {
+    stateStore.snippetDialogVisible = true
+  }
 }
 // markdown 上传图片
 const uploadImgEventFunction = async (files: Array<File>, callback: (urls: Array<string>) => void) => {
-    const form = new FormData();
-    // 组合表单对象批量上传
-    for (const file of files) {
-        form.append('files', file);
-    }
-    // 上传图片
-    const res = await uploadImg(form)
-    // 响应md
-    callback(res.data.urls)
+  const form = new FormData();
+  // 组合表单对象批量上传
+  for (const file of files) {
+    form.append('files', file);
+  }
+  // 上传图片
+  const res = await uploadImg(form)
+  // 响应md
+  callback(res.data.urls)
 }
 const instance = getCurrentInstance()
 // 注册监听切换面板事件
 instance?.proxy?.$bus.on(SNIPPET_GET_EVENT, (snippet) => {
-    snippetForm.value = snippet as SnippetType
+  snippetForm.value = snippet as SnippetType
 })
 
-onBeforeMount(() => {
-    // 卸载事件函数
-    instance?.proxy?.$bus.off(SNIPPET_GET_EVENT)
+onBeforeUnmount(() => {
+  // 卸载事件函数
+  instance?.proxy?.$bus.off(SNIPPET_GET_EVENT)
 })
 </script>
 <style scoped lang="less">
