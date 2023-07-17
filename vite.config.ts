647b1d3f885b7b6d3eb1f524ad8d2f244dc6f9ae
HMETAO <1430271732@qq.com>
2023-07-18 00:02:14 +0800

    fix(config): 增加gzip优化

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import Components from 'unplugin-vue-components/vite'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 // vite.config.js/ts
 import {join} from "path";
+import compressPlugin from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,13 @@ export default defineConfig({
     },
     plugins: [
         vue(),
+        compressPlugin({ //gzip静态资源压缩
+            verbose: true,    // 默认即可
+            disable: false,  //开启压缩(不禁用)，默认即可
+            deleteOriginFile: false, //删除源文件
+            algorithm: 'gzip',  //压缩算法
+            ext: '.gz', //文件类型
+        }),
         // ...
         AutoImport({
             resolvers: [ElementPlusResolver()],
